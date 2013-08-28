f3702307c39e412d9f7415d122697774892d90e2
Discover- <amort11@hotmail.com>
2013-08-28 13:19:49 +0200

    Rename LoginForm to MainForm. From now on connecting to the database expands the form with a simple 'effect' (expanding by timer)

---
+++ SAI-Editor/MainForm.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace SAI_Editor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.labelUser = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelHost = new System.Windows.Forms.Label();
+            this.labelPort = new System.Windows.Forms.Label();
+            this.checkBoxSaveSettings = new System.Windows.Forms.CheckBox();
+            this.textBoxHost = new System.Windows.Forms.TextBox();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.textBoxWorldDatabase = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.groupBoxLogin = new System.Windows.Forms.GroupBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.groupBoxCreatureInfo = new System.Windows.Forms.GroupBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.testToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.testToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.test2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.test3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.testderpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dawdaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.awdawToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBoxLogin.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelUser
+            // 
+            this.labelUser.AutoSize = true;
+            this.labelUser.Location = new System.Drawing.Point(34, 48);
+            this.labelUser.Name = "labelUser";
+            this.labelUser.Size = new System.Drawing.Size(58, 13);
+            this.labelUser.TabIndex = 0;
+            this.labelUser.Text = "Username:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Password:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "World Database:";
+            // 
+            // labelHost
+            // 
+            this.labelHost.AutoSize = true;
+            this.labelHost.Location = new System.Drawing.Point(60, 22);
+            this.labelHost.Name = "labelHost";
+            this.labelHost.Size = new System.Drawing.Size(32, 13);
+            this.labelHost.TabIndex = 3;
+            this.labelHost.Text = "Host:";
+            // 
+            // labelPort
+            // 
+            this.labelPort.AutoSize = true;
+            this.labelPort.Location = new System.Drawing.Point(63, 126);
+            this.labelPort.Name = "labelPort";
+            this.labelPort.Size = new System.Drawing.Size(29, 13);
+            this.labelPort.TabIndex = 4;
+            this.labelPort.Text = "Port:";
+            // 
+            // checkBoxSaveSettings
+            // 
+            this.checkBoxSaveSettings.AutoSize = true;
+            this.checkBoxSaveSettings.Checked = true;
+            this.checkBoxSaveSettings.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxSaveSettings.Location = new System.Drawing.Point(98, 149);
+            this.checkBoxSaveSettings.Name = "checkBoxSaveSettings";
+            this.checkBoxSaveSettings.Size = new System.Drawing.Size(90, 17);
+            this.checkBoxSaveSettings.TabIndex = 5;
+            this.checkBoxSaveSettings.Text = "Save settings";
+            this.checkBoxSaveSettings.UseVisualStyleBackColor = true;
+            // 
+            // textBoxHost
+            // 
+            this.textBoxHost.Location = new System.Drawing.Point(98, 19);
+            this.textBoxHost.Name = "textBoxHost";
+            this.textBoxHost.Size = new System.Drawing.Size(130, 20);
+            this.textBoxHost.TabIndex = 0;
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(98, 123);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(130, 20);
+            this.textBoxPort.TabIndex = 4;
+            // 
+            // textBoxWorldDatabase
+            // 
+            this.textBoxWorldDatabase.Location = new System.Drawing.Point(98, 97);
+            this.textBoxWorldDatabase.Name = "textBoxWorldDatabase";
+            this.textBoxWorldDatabase.Size = new System.Drawing.Size(130, 20);
+            this.textBoxWorldDatabase.TabIndex = 3;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(98, 71);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(130, 20);
+            this.textBoxPassword.TabIndex = 2;
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(98, 45);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(130, 20);
+            this.textBoxUsername.TabIndex = 1;
+            // 
+            // groupBoxLogin
+            // 
+            this.groupBoxLogin.Controls.Add(this.textBoxHost);
+            this.groupBoxLogin.Controls.Add(this.textBoxUsername);
+            this.groupBoxLogin.Controls.Add(this.labelUser);
+            this.groupBoxLogin.Controls.Add(this.textBoxPassword);
+            this.groupBoxLogin.Controls.Add(this.label1);
+            this.groupBoxLogin.Controls.Add(this.textBoxWorldDatabase);
+            this.groupBoxLogin.Controls.Add(this.label2);
+            this.groupBoxLogin.Controls.Add(this.textBoxPort);
+            this.groupBoxLogin.Controls.Add(this.labelHost);
+            this.groupBoxLogin.Controls.Add(this.labelPort);
+            this.groupBoxLogin.Controls.Add(this.checkBoxSaveSettings);
+            this.groupBoxLogin.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxLogin.Name = "groupBoxLogin";
+            this.groupBoxLogin.Size = new System.Drawing.Size(237, 172);
+            this.groupBoxLogin.TabIndex = 10;
+            this.groupBoxLogin.TabStop = false;
+            this.groupBoxLogin.Text = "Connect information";
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(12, 190);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnect.TabIndex = 6;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(93, 190);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 7;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(174, 190);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 8;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // groupBoxCreatureInfo
+            // 
+            this.groupBoxCreatureInfo.Location = new System.Drawing.Point(12, 31);
+            this.groupBoxCreatureInfo.Name = "groupBoxCreatureInfo";
+            this.groupBoxCreatureInfo.Size = new System.Drawing.Size(272, 160);
+            this.groupBoxCreatureInfo.TabIndex = 6;
+            this.groupBoxCreatureInfo.TabStop = false;
+            this.groupBoxCreatureInfo.Text = "Creature information";
+            this.groupBoxCreatureInfo.Visible = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.testToolStripMenuItem,
+            this.test2ToolStripMenuItem,
+            this.test3ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1045, 24);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.Visible = false;
+            // 
+            // testToolStripMenuItem
+            // 
+            this.testToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.testderpToolStripMenuItem});
+            this.testToolStripMenuItem.Name = "testToolStripMenuItem";
+            this.testToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.testToolStripMenuItem.Text = "Test";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.testToolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(94, 26);
+            // 
+            // testToolStripMenuItem1
+            // 
+            this.testToolStripMenuItem1.Name = "testToolStripMenuItem1";
+            this.testToolStripMenuItem1.Size = new System.Drawing.Size(93, 22);
+            this.testToolStripMenuItem1.Text = "test";
+            // 
+            // test2ToolStripMenuItem
+            // 
+            this.test2ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dawdaToolStripMenuItem});
+            this.test2ToolStripMenuItem.Name = "test2ToolStripMenuItem";
+            this.test2ToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.test2ToolStripMenuItem.Text = "Test2";
+            // 
+            // test3ToolStripMenuItem
+            // 
+            this.test3ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.awdawToolStripMenuItem});
+            this.test3ToolStripMenuItem.Name = "test3ToolStripMenuItem";
+            this.test3ToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.test3ToolStripMenuItem.Text = "Test3";
+            // 
+            // testderpToolStripMenuItem
+            // 
+            this.testderpToolStripMenuItem.Name = "testderpToolStripMenuItem";
+            this.testderpToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.testderpToolStripMenuItem.Text = "Testderp";
+            // 
+            // dawdaToolStripMenuItem
+            // 
+            this.dawdaToolStripMenuItem.Name = "dawdaToolStripMenuItem";
+            this.dawdaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.dawdaToolStripMenuItem.Text = "dawda";
+            // 
+            // awdawToolStripMenuItem
+            // 
+            this.awdawToolStripMenuItem.Name = "awdawToolStripMenuItem";
+            this.awdawToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.awdawToolStripMenuItem.Text = "awdaw";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(898, 519);
+            this.Controls.Add(this.groupBoxCreatureInfo);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.groupBoxLogin);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "SAI Editor | Login";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.groupBoxLogin.ResumeLayout(false);
+            this.groupBoxLogin.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelUser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelHost;
+        private System.Windows.Forms.Label labelPort;
+        private System.Windows.Forms.CheckBox checkBoxSaveSettings;
+        private System.Windows.Forms.TextBox textBoxHost;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.TextBox textBoxWorldDatabase;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.GroupBox groupBoxLogin;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.GroupBox groupBoxCreatureInfo;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem testToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem testToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem testderpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem test2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dawdaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem test3ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem awdawToolStripMenuItem;
+    }
+}
+
--- SAI-Editor/Program.cs
+++ SAI-Editor/Program.cs
@@ -16,7 +16,7 @@ namespace SAI_Editor
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm());
+            Application.Run(new MainForm());
         }
     }
 }
