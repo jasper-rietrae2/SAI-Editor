9ca461ce945967d3d1d6dd80853e06899fa5c2e3
Discover- <amort11@hotmail.com>
2013-08-31 19:01:15 +0200

    Prompt the user if he wishes to save his the settings when closing the Settings form while having unsaved setting changes

--- SAI-Editor/SettingsForm.cs
+++ SAI-Editor/SettingsForm.cs
@@ -5,6 +5,8 @@ namespace SAI_Editor
 {
     public partial class SettingsForm : Form
     {
+        private Settings settings = null;
+
         public SettingsForm()
         {
             InitializeComponent();
@@ -14,32 +16,58 @@ namespace SAI_Editor
         {
             MaximizeBox = false;
             MinimizeBox = true;
+            FormClosed += SettingsForm_FormClosed;
+            settings = ((MainForm)Owner).settings;
 
-            textBoxHost.Text = ((MainForm)Owner).settings.GetSetting("Host", "localhost");
-            textBoxUsername.Text = ((MainForm)Owner).settings.GetSetting("User", "root");
-            textBoxPassword.Text = ((MainForm)Owner).settings.GetSetting("Password", string.Empty);
-            textBoxWorldDatabase.Text = ((MainForm)Owner).settings.GetSetting("Database", "trinitycore_world");
-            textBoxPort.Text = ((MainForm)Owner).settings.GetSetting("Port", "3306");
-            checkBoxAutoLogin.Checked = ((MainForm)Owner).settings.GetSetting("Autologin", "no") == "yes";
-            checkBoxExpandInstantly.Checked = ((MainForm)Owner).settings.GetSetting("InstantExpand", "no") == "yes";
+            textBoxHost.Text = settings.GetSetting("Host", "localhost");
+            textBoxUsername.Text = settings.GetSetting("User", "root");
+            textBoxPassword.Text = settings.GetSetting("Password", String.Empty);
+            textBoxWorldDatabase.Text = settings.GetSetting("Database", "trinitycore_world");
+            textBoxPort.Text = settings.GetSetting("Port", "3306");
+            checkBoxAutoLogin.Checked = settings.GetSetting("Autologin", "no") == "yes";
+            checkBoxExpandInstantly.Checked = settings.GetSetting("InstantExpand", "no") == "yes";
         }
 
         private void buttonSaveSettings_Click(object sender, EventArgs e)
         {
-            ((MainForm)Owner).settings.PutSetting("Host", textBoxHost.Text);
-            ((MainForm)Owner).settings.PutSetting("User", textBoxUsername.Text);
-            ((MainForm)Owner).settings.PutSetting("Password", textBoxPassword.Text);
-            ((MainForm)Owner).settings.PutSetting("Database", textBoxWorldDatabase.Text);
-            ((MainForm)Owner).settings.PutSetting("Port", textBoxPort.Text);
-            ((MainForm)Owner).settings.PutSetting("Autologin", (checkBoxAutoLogin.Checked ? "yes" : "no"));
-            ((MainForm)Owner).settings.PutSetting("InstantExpand", (checkBoxExpandInstantly.Checked ? "yes" : "no"));
-            ((MainForm)Owner).checkBoxAutoLogin.Checked = checkBoxAutoLogin.Checked;
+            SaveSettings();
             Close();
+        }
+
+        private void SaveSettings()
+        {
+            settings.PutSetting("Host", textBoxHost.Text);
+            settings.PutSetting("User", textBoxUsername.Text);
+            settings.PutSetting("Password", textBoxPassword.Text);
+            settings.PutSetting("Database", textBoxWorldDatabase.Text);
+            settings.PutSetting("Port", textBoxPort.Text);
+            settings.PutSetting("Autologin", (checkBoxAutoLogin.Checked ? "yes" : "no"));
+            settings.PutSetting("InstantExpand", (checkBoxExpandInstantly.Checked ? "yes" : "no"));
+            checkBoxAutoLogin.Checked = checkBoxAutoLogin.Checked;
         }
 
         private void buttonExitSettings_Click(object sender, EventArgs e)
         {
+            PromptSaveSettingsOnClose();
             Close();
+        }
+
+        void SettingsForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+                PromptSaveSettingsOnClose();
+        }
+
+        void PromptSaveSettingsOnClose()
+        {
+            if (textBoxHost.Text == settings.GetSetting("Host", "localhost") && textBoxUsername.Text == settings.GetSetting("User", "root") &&
+                textBoxPassword.Text == settings.GetSetting("Password", String.Empty) && textBoxWorldDatabase.Text == settings.GetSetting("Database", "trinitycore_world") &&
+                textBoxPort.Text == settings.GetSetting("Port", "3306") && checkBoxAutoLogin.Checked == (settings.GetSetting("Autologin", "no") == "yes") &&
+                checkBoxExpandInstantly.Checked == (settings.GetSetting("InstantExpand", "no") == "yes"))
+                return;
+
+            if (MessageBox.Show("Do you wish to save the edited settings?", "Save settings?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                SaveSettings();
         }
 
         private void buttonClearSettings_Click(object sender, EventArgs e)
