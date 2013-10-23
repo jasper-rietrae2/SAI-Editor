d7820bbbb34e9c3c4534a461caa1d8053a6b9d86
Discover- <amort11@hotmail.com>
2013-10-23 07:47:00 +0200

    Correct the namespace used by ForceSelectListView (and add files that I forgot to add in previous commits)

---
+++ SAI-Editor/Classes/DetailedToolTip.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SAI_Editor.Classes
+{
+    public class DetailedToolTip : ToolTip
+    {
+        public void SetToolTipText(Control control, string caption)
+        {
+            string newCaption = "";
+
+            if (caption.Length > 80)
+            {
+                string[] splitCaption = caption.Split(Convert.ToChar(" "));
+
+                int totalLength = 0;
+                List<string> wordsSoFar = new List<string>();
+                List<List<string>> captionChunks = new List<List<string>>();
+
+                for (int i = 0; i < splitCaption.Length; ++i)
+                {
+                    //! +1 because of the whitespace that was lost in the Split call.
+                    totalLength += splitCaption[i].Length + 1;
+                    wordsSoFar.Add(splitCaption[i]);
+
+                    if (totalLength > 60 || i == splitCaption.Length - 1)
+                    {
+                        List<string> wordsSoFarCopy = new List<string>(wordsSoFar);
+                        captionChunks.Add(wordsSoFarCopy);
+                        wordsSoFar.Clear();
+                        totalLength = 0;
+                    }
+                }
+
+                foreach (List<string> listString in captionChunks)
+                {
+                    foreach (string strPart in listString)
+                        newCaption += strPart + " ";
+
+                    newCaption += "\n";
+                }
+            }
+            else
+                newCaption = caption;
+
+            //! We can't use this because it will cut off words, which is exactly what we're trying to avoid.
+            //List<string> strParts = new List<string>(Regex.Split(caption, @"(?<=\G.{60})", RegexOptions.Singleline));
+
+            SetToolTip(control, newCaption);
+        }
+    }
+}
---
+++ SAI-Editor/Classes/ForceSelectListView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SAI_Editor.Classes
+{
+    public class ForceSelectListView : ListView
+    {
+        protected override void WndProc(ref Message m)
+        {
+            //! Disallows de-selecting an item in the listview
+            if (m.Msg >= 0x201 && m.Msg <= 0x209)
+            {
+                Point pos = new Point(m.LParam.ToInt32() & 0xffff, m.LParam.ToInt32() >> 16);
+                var hit = this.HitTest(pos);
+                switch (hit.Location)
+                {
+                    case ListViewHitTestLocations.AboveClientArea:
+                    case ListViewHitTestLocations.BelowClientArea:
+                    case ListViewHitTestLocations.LeftOfClientArea:
+                    case ListViewHitTestLocations.RightOfClientArea:
+                    case ListViewHitTestLocations.None:
+                        return;
+                }
+            }
+
+            base.WndProc(ref m);
+        }
+    }
+}
