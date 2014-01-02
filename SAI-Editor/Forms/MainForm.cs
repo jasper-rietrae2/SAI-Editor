c113f13ddad5b5c50c3a88303d0b45de47f92d29
ainarsh@gmail.com <ainarsh@gmail.com>
2014-01-02 21:05:23 +0200

    ﻿Using directive is not required by the code and can be safely removed

--- SAI-Editor/Forms/MainForm.cs
+++ SAI-Editor/Forms/MainForm.cs
@@ -5,7 +5,6 @@ using System.Drawing;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
