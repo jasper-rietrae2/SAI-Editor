ff55d300086aa19670ff3d968e5634a93d9cfe23
ainarsh@gmail.com <ainarsh@gmail.com>
2014-01-05 14:20:38 +0200

    Convert to auto-property

--- SAI-Editor/Classes/SmartScriptListViewItem.cs
+++ SAI-Editor/Classes/SmartScriptListViewItem.cs
@@ -6,16 +6,11 @@ namespace SAI_Editor.Classes
 {
     public class SmartScriptListViewItem : ListViewItem
     {
-        private SmartScript _script;
         private Color _lastBackColor = Color.White;
 
         public SmartScriptListViewItem(string text) : base(text) { }
 
-        public SmartScript Script
-        {
-            get { return _script; }
-            set { _script = value; }
-        }
+        public SmartScript Script { get; set; }
 
         public Color LastBackColor
         {
