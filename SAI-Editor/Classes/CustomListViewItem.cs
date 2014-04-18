8a40df28cc3dc3239fe230a521fe72b90c23c264
Discover- <amort11@hotmail.com>
2014-04-18 22:04:37 +0200

    Add missing files for previous commits

---
+++ SAI-Editor/Classes/CustomListViewItem.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using SAI_Editor.Classes.Database.Classes;
+
+namespace SAI_Editor.Classes
+{
+    public class CustomListViewItem : ListViewItem
+    {
+        public Color LastBackColor = Color.White;
+        public DatabaseClass Script;
+
+        public CustomListViewItem(string text/*, DatabaseClass scriptClass*/) : base(text)
+        {
+            //Script = scriptClass;
+        }
+    }
+}
---
+++ SAI-Editor/Classes/Database/Classes/DatabaseClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAI_Editor.Classes.Database.Classes
+{
+    public class DatabaseClass
+    {
+    }
+}
