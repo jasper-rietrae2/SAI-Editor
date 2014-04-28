300d4e379dd2d731417095c7f64239d7bdde5901
Mitchell Kutchuk <mitch528@gmail.com>
2014-04-28 10:22:31 -0700

    After resize, controls should now appear faster

---
+++ SAI-Editor/Classes/CustomControls/CustomPanel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SAI_Editor.Classes.CustomControls
+{
+    public class CustomPanel : Panel
+    {
+
+        public CustomPanel()
+        {
+            SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.UserPaint, true);
+            ResizeRedraw = true;
+        }
+
+        protected override bool DoubleBuffered
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+    }
+}
