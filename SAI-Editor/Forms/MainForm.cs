jasper-rietrae2/SAI-Editor

commit eef2df6f7efb0e8af7d78196b6a08b9fa4496e26

Fix printing of coordinate values (target_x/y/z/o fields). Reverts aec51cabd50f54e33dd46e94f5eeecf645d63459 (damn, that's an old issue..)
Thanks to Polkic for the notice!

SAI-Editor/Forms/MainForm.cs
@@ -3377,7 +3377,7 @@ namespace SAI_Editor.Forms
         {
             if (listViewSmartScripts.SelectedItems.Count > 0)
             {
-                textBoxTargetX.Text = textBoxTargetX.Text.Replace(",", ".");
+                textBoxTargetX.Text = textBoxTargetX.Text.Replace(".", ",");
                 textBoxTargetX.SelectionStart = textBoxTargetX.Text.Length + 1; //! Set cursor to end of text
                 listViewSmartScripts.SelectedSmartScript.target_x = XConverter.ToDouble(textBoxTargetX.Text);
                 listViewSmartScripts.ReplaceSmartScript(listViewSmartScripts.SelectedSmartScript);
@@ -3389,7 +3389,7 @@ namespace SAI_Editor.Forms
         {
             if (listViewSmartScripts.SelectedItems.Count > 0)
             {
-                textBoxTargetY.Text = textBoxTargetY.Text.Replace(",", ".");
+                textBoxTargetY.Text = textBoxTargetY.Text.Replace(".", ",");
                 textBoxTargetY.SelectionStart = textBoxTargetY.Text.Length + 1; //! Set cursor to end of text
                 listViewSmartScripts.SelectedSmartScript.target_y = XConverter.ToDouble(textBoxTargetY.Text);
                 listViewSmartScripts.ReplaceSmartScript(listViewSmartScripts.SelectedSmartScript);
@@ -3401,7 +3401,7 @@ namespace SAI_Editor.Forms
         {
             if (listViewSmartScripts.SelectedItems.Count > 0)
             {
-                textBoxTargetZ.Text = textBoxTargetZ.Text.Replace(",", ".");
+                textBoxTargetZ.Text = textBoxTargetZ.Text.Replace(".", ",");
                 textBoxTargetZ.SelectionStart = textBoxTargetZ.Text.Length + 1; //! Set cursor to end of text
                 listViewSmartScripts.SelectedSmartScript.target_z = XConverter.ToDouble(textBoxTargetZ.Text);
                 listViewSmartScripts.ReplaceSmartScript(listViewSmartScripts.SelectedSmartScript);
@@ -3413,7 +3413,7 @@ namespace SAI_Editor.Forms
         {
             if (listViewSmartScripts.SelectedItems.Count > 0)
             {
-                textBoxTargetO.Text = textBoxTargetO.Text.Replace(",", ".");
+                textBoxTargetO.Text = textBoxTargetO.Text.Replace(".", ",");
                 textBoxTargetO.SelectionStart = textBoxTargetO.Text.Length + 1; //! Set cursor to end of text
                 listViewSmartScripts.SelectedSmartScript.target_o = XConverter.ToDouble(textBoxTargetO.Text);
                 listViewSmartScripts.ReplaceSmartScript(listViewSmartScripts.SelectedSmartScript);
@@ -3759,13 +3759,19 @@ namespace SAI_Editor.Forms
                             targetParameters[x] = XConverter.ToInt32(sourceSet);
                 }
 
+                //! SQL accepts a period instead of a comma for float/double values
+                string target_x = smartScript.target_x.ToString().Replace(",", ".");
+                string target_y = smartScript.target_y.ToString().Replace(",", ".");
+                string target_z = smartScript.target_z.ToString().Replace(",", ".");
+                string target_o = smartScript.target_o.ToString().Replace(",", ".");
+
                 generatedSql += "(" + actualSourceSet + "," + smartScript.source_type + "," + smartScript.id + "," + smartScript.link + "," + smartScript.event_type + "," +
                                               smartScript.event_phase_mask + "," + smartScript.event_chance + "," + smartScript.event_flags + "," + eventParameters[0] + "," +
                                               eventParameters[1] + "," + eventParameters[2] + "," + eventParameters[3] + "," + smartScript.action_type + "," +
                                               actionParameters[0] + "," + actionParameters[1] + "," + actionParameters[2] + "," + actionParameters[3] + "," +
                                               actionParameters[4] + "," + actionParameters[5] + "," + smartScript.target_type + "," + targetParameters[0] + "," +
-                                              targetParameters[1] + "," + targetParameters[2] + "," + smartScript.target_x + "," + smartScript.target_y + "," +
-                                              smartScript.target_z + "," + smartScript.target_o + "," + '"' + smartScript.comment + '"' + ")";
+                                              targetParameters[1] + "," + targetParameters[2] + "," + target_x + "," + target_y + "," + target_z + "," + target_o + "," +
+                                              '"' + smartScript.comment + '"' + ")";
 
                 if (i == smartScripts.Count - 1)
                     generatedSql += ";";

SAI-Editor/Classes/Database/WorldDatabase.cs
@@ -415,10 +415,10 @@ namespace SAI_Editor.Classes.Database
             smartScript.target_param1 = row["target_param1"] != DBNull.Value ? XConverter.ToInt32(row["target_param1"]) : 0;
             smartScript.target_param2 = row["target_param2"] != DBNull.Value ? XConverter.ToInt32(row["target_param2"]) : 0;
             smartScript.target_param3 = row["target_param3"] != DBNull.Value ? XConverter.ToInt32(row["target_param3"]) : 0;
-            smartScript.target_x = row["target_x"] != DBNull.Value ? XConverter.ToInt32(row["target_x"]) : 0;
-            smartScript.target_y = row["target_y"] != DBNull.Value ? XConverter.ToInt32(row["target_y"]) : 0;
-            smartScript.target_z = row["target_z"] != DBNull.Value ? XConverter.ToInt32(row["target_z"]) : 0;
-            smartScript.target_o = row["target_o"] != DBNull.Value ? XConverter.ToInt32(row["target_o"]) : 0;
+            smartScript.target_x = row["target_x"] != DBNull.Value ? XConverter.ToDouble(row["target_x"]) : 0;
+            smartScript.target_y = row["target_y"] != DBNull.Value ? XConverter.ToDouble(row["target_y"]) : 0;
+            smartScript.target_z = row["target_z"] != DBNull.Value ? XConverter.ToDouble(row["target_z"]) : 0;
+            smartScript.target_o = row["target_o"] != DBNull.Value ? XConverter.ToDouble(row["target_o"]) : 0;
             smartScript.comment = row["comment"] != DBNull.Value ? (string)row["comment"] : String.Empty;
             return smartScript;
         }

SAI-Editor/Classes/XConverter.cs
@@ -4,24 +4,10 @@ namespace SAI_Editor.Classes
 {
     static class XConverter
     {
-        public static Int32 ToInt32(string str)
-        {
-            int output;
-            Int32.TryParse(str, out output);
-            return output;
-        }
-
         public static Int32 ToInt32(object str)
         {
             int output;
             Int32.TryParse(str.ToString(), out output);
-            return output;
-        }
-
-        public static UInt32 ToUInt32(string str)
-        {
-            uint output;
-            UInt32.TryParse(str, out output);
             return output;
         }
 
@@ -32,23 +18,10 @@ namespace SAI_Editor.Classes
             return output;
         }
 
-        public static long ToInt64(string str)
-        {
-            long output;
-            long.TryParse(str, out output);
-            return output;
-        }
-
         public static long ToInt64(object str)
         {
             long output;
             long.TryParse(str.ToString(), out output);
-            return output;
-        }
-        public static double ToDouble(string str)
-        {
-            double output;
-            Double.TryParse(str, out output);
             return output;
         }
 
@@ -56,6 +29,13 @@ namespace SAI_Editor.Classes
         {
             double output;
             Double.TryParse(str.ToString(), out output);
+            return output;
+        }
+
+        public static float ToFloat(object str)
+        {
+            float output;
+            float.TryParse(str.ToString(), out output);
             return output;
         }
     }

commit c045aa9ef1ce187cdbe751b15da5deceed599826

Fix a new exception thrown when adding a second item to the listview. Caused by eef2df6f7efb0e8af7d78196b6a08b9fa4496e26 because strings do not, unlike ints, get a default value such as an empty string but rather become null. Thanks to @Mitch528!

SAI-Editor/Forms/MainForm.cs
@@ -2308,36 +2308,17 @@ namespace SAI_Editor.Forms
             }
         }
 
-        private void SetTextOfAllEventParameterLabels(string str)
-        {
-            labelEventParam1.Text = str;
-            labelEventParam2.Text = str;
-            labelEventParam3.Text = str;
-            labelEventParam4.Text = str;
-        }
-
-        private void SetTextOfAllActionParameterLabels(string str)
-        {
-            labelActionParam1.Text = str;
-            labelActionParam2.Text = str;
-            labelActionParam3.Text = str;
-            labelActionParam4.Text = str;
-            labelActionParam5.Text = str;
-            labelActionParam6.Text = str;
-        }
-
-        private void SetTextOfAllTargetParameterLabels(string str)
-        {
-            labelTargetParam1.Text = str;
-            labelTargetParam2.Text = str;
-            labelTargetParam3.Text = str;
-        }
-
         private void ParameterInstallAiTemplateChanged()
         {
             SetVisibilityOfAllParamButtonsInTab("Action", false);
             SetVisibilityOfAllParamButtonsInTab("Target", false);
-            SetTextOfAllActionParameterLabels(String.Empty);
+
+            labelActionParam1.Text = String.Empty;
+            labelActionParam2.Text = String.Empty;
+            labelActionParam3.Text = String.Empty;
+            labelActionParam4.Text = String.Empty;
+            labelActionParam5.Text = String.Empty;
+            labelActionParam6.Text = String.Empty;
 
             labelActionParam1.Text = "Template entry";
             buttonActionParamOneSearch.Visible = true;
@@ -2724,6 +2705,12 @@ namespace SAI_Editor.Forms
                 newSmartScript.comment = SAI_Editor_Manager.Instance.GetDefaultCommentForSourceType((SourceTypes)newSmartScript.source_type);
 
             newSmartScript.event_chance = 100;
+
+            //! All strings have to be initialized otherwise they become null and give cause an exception in the future.
+            newSmartScript.target_x = String.Empty;
+            newSmartScript.target_y = String.Empty;
+            newSmartScript.target_z = String.Empty;
+            newSmartScript.target_o = String.Empty;
             int index = listViewSmartScripts.AddSmartScript(newSmartScript);
             HandleShowBasicInfo();
 