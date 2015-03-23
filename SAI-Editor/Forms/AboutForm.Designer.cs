f188625efc47e5b5c59967620d53e8429f1bc983
Discover- <amort11@hotmail.com>
2015-03-23 20:20:25 +0100

    Update the TODO and the AboutForm

--- SAI-Editor/Forms/AboutForm.Designer.cs
+++ SAI-Editor/Forms/AboutForm.Designer.cs
@@ -97,7 +97,7 @@
             this.labelCompanyName.Name = "labelCompanyName";
             this.labelCompanyName.Size = new System.Drawing.Size(271, 17);
             this.labelCompanyName.TabIndex = 22;
-            this.labelCompanyName.Text = "Written by Jasper \'Discover-\' R.";
+            this.labelCompanyName.Text = "Written by Discover- & Mitch528";
             this.labelCompanyName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // textBoxDescription
