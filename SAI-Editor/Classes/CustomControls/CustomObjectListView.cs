0b1c49074674e8670702f3ee2dd185ad29ac73c5
Discover- <amort11@hotmail.com>
2015-05-20 01:29:21 +0200

    It is no longer possible to de-select an item in the listview

--- SAI-Editor/Classes/CustomControls/CustomObjectListView.cs
+++ SAI-Editor/Classes/CustomControls/CustomObjectListView.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using BrightIdeasSoftware;
 
 namespace SAI_Editor.Classes.CustomControls
@@ -10,5 +12,26 @@ namespace SAI_Editor.Classes.CustomControls
     public class CustomObjectListView : FastObjectListView
     {
         public CList List { get; set; }
+
+        protected override void WndProc(ref Message m)
+        {
+            //! Disallows de-selecting an item in the listview
+            if (m.Msg >= 0x201 && m.Msg <= 0x209)
+            {
+                Point pos = new Point(m.LParam.ToInt32() & 0xffff, m.LParam.ToInt32() >> 16);
+                var hit = this.HitTest(pos);
+                switch (hit.Location)
+                {
+                    case ListViewHitTestLocations.AboveClientArea:
+                    case ListViewHitTestLocations.BelowClientArea:
+                    case ListViewHitTestLocations.LeftOfClientArea:
+                    case ListViewHitTestLocations.RightOfClientArea:
+                    case ListViewHitTestLocations.None:
+                        return;
+                }
+            }
+
+            base.WndProc(ref m);
+        }
     }
 }
